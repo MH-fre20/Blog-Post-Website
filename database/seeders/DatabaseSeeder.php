b0aa0a7176bdefff66995d7cc9f1392af962f0ef
mohamad <mohammadjafer@gmail.com>
2022-06-04 11:54:37 +0300

    database seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Comment;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
@@ -18,12 +19,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+
         DB::table('users')->insert([
-            'name' => Str::random(10),
-            'email' => Str::random(10).'@gmail.com',
-            'address' => Str::random(10)
+            'name' => 'phpunit',
+            'email' => 'phpunit@gmail.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
         ]);
         // \App\Models\User::factory(10)->create();
         //factory(App\User::class, 20)->create;
+        User::factory()->count(5)->create();
     }
 }
