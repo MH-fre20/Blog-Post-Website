fdffd2e5d622e4ed24a91727e66f1aa91dfff857
mohamad <mohammadjafer@gmail.com>
2022-06-13 17:34:00 +0300

    the test after authorization is working

--- resources/views/secret.blade.php
+++ resources/views/secret.blade.php
@@ -1,7 +1,8 @@
 @extends('layout.app')
 
 @section('content')
-    <p class="p-2 m-4 display-6">hello admin my old friend i have come to see you
+    <p class="p-2 m-4 display-6">
+        hello admin my old friend i have come to see you
         again
     </p>
 @endsection
--- tests/Feature/PostTest.php
+++ tests/Feature/PostTest.php
@@ -20,7 +20,7 @@ class PostTest extends TestCase
      /** @test */
     public function test_Post_Test()
     {
-        /*  $user = User::factory()->create();
+        $user = User::factory()->create();
         $post = BlogPost::factory(['user_id' => $user->id])->count(4);
         $post = new BlogPost();
         $post->title = 'New title';
@@ -29,17 +29,17 @@ class PostTest extends TestCase
         $post->save();
 
         $this->assertSame($user->id, $post->user_id);
-        //to be sure about the post  belongsTo a user
-        $this->assertInstanceOf(User::class, $post->user); */
+        //to be sure about the post belongsTo a user
+        $this->assertInstanceOf(User::class, $post->user); 
         //$this->assertInstanceOf(HasMany::class, $post);
         //method 2
 
         //to see if we have BlogPost function in user class
         /* $this->assertEquals(1, $user->BlogPost->count()); */
-
         $post = new BlogPost();
         $post->title = 'New title';
         $post->content = 'the content';
+        $post->user_id = $user->id;
         $post->save();
 
         $hello = 'hello';
@@ -63,9 +63,11 @@ class PostTest extends TestCase
 
     public function test_see_blog_post()
     {
+        $user = User::factory()->create();
         $post = new BlogPost();
         $post->title = 'hellow';
         $post->content = 'content';
+        $post->user_id = $user->id;
         $post->save();
 
         $object = $this->get('/posts');
@@ -78,9 +80,11 @@ class PostTest extends TestCase
 
     public function test_in_database()
     {
+        $user = User::factory()->create();
         $post = new BlogPost();
         $post->title = 'hellow';
         $post->content = 'content';
+        $post->user_id = $user->id;
         $post->save();
 
         $this->get('/posts')->assertSeeText('hellow');
@@ -106,9 +110,11 @@ class PostTest extends TestCase
 
     public function test_if_updated()
     {
+        $user = User::factory()->create();
         $post = new BlogPost();
         $post->title = 'hellow';
         $post->content = 'content';
+        $post->user_id = $user->id;
         $post->save();
 
         $this->assertDatabaseHas('blog_posts', [
@@ -126,9 +132,11 @@ class PostTest extends TestCase
 
     public function test_if_deleted()
     {
+        $user = User::factory()->create();
         $post = new BlogPost();
         $post->title = 'hellow';
         $post->content = 'content';
+        $post->user_id = $user->id;
         $post->save();
 
         $this->delete('/posts/{$post->id}')
